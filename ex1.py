9bb999e8abe9fe5beda5b13cd3463b0648398e46
Carl Elgario <kirstencarl.elgario@ucalgary>
2024-03-04 23:39:24 -0700

    ex1.py in progress

--- ex1.py
+++ ex1.py
@@ -1,3 +1,5 @@
+import sys
+
 class Node:
     def __init__(self, data=None):
         self.data = data
@@ -29,3 +31,47 @@ class Stack:
 
     def is_empty(self):
         return self.top is None
+
+def parse_sexpr(tokens):
+    stack = Stack()
+
+    while tokens:
+        token = tokens.pop(0)
+        if token.isdigit():
+            stack.push(int(token))
+        elif token in ['+', '-', '*', '/']:
+            operands = []
+            while not stack.is_empty() and isinstance(stack.peek(), int):
+                operands.append(stack.pop())
+            if token == '+':
+                stack.push(sum(operands))
+            elif token == '-':
+                stack.push(operands[0] - sum(operands[1:]))
+            elif token == '*':
+                result = 1
+                for operand in operands:
+                    result *= operand
+                stack.push(result)
+            elif token == '/':
+                stack.push(operands[0] / operands[1])
+        elif token == '(':
+            # Start a new recursive call to parse the sub-expression
+            stack.push(parse_sexpr(tokens))
+        elif token == ')':
+            # End the current recursive call and return the result
+            if not stack.is_empty():
+                return stack.pop()
+
+    # Return the result of the entire expression
+    if not stack.is_empty():
+        return stack.pop()
+    else:
+        return None
+
+if len(sys.argv) > 1:
+    expr = [i for i in sys.argv[1]]
+    print(expr)
+    result = parse_sexpr(expr)
+    print(result)
+else:
+    print("No command line parameter provided")
